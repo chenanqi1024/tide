ff45b6b9de1bea909545cea6b2389d5f3152a06e
chenanqi1024 <chenanqi1024@gmail.com>
2026-06-04 13:14:10 +0800

    Initial Commit

---
+++ tide/tideApp.swift
@@ -0,0 +1,17 @@
+//
+//  tideApp.swift
+//  tide
+//
+//  Created by chenanqi on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct tideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
